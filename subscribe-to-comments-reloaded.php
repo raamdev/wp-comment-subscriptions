6c11b53b6df3d03419b9e548702ac5801f26e9ff
Raam Dev <raam@raamdev.com>
2013-07-26 12:19:54 -0400

    Wrap StCR html in a <p> tag with 'comment-form-subscriptions' class

--- subscribe-to-comments-reloaded.php
+++ subscribe-to-comments-reloaded.php
@@ -78,7 +78,7 @@ function subscribe_reloaded_show(){
 		}
 	}
 	if(function_exists('qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage')) $html_to_show = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($html_to_show);
-	echo "<!-- BEGIN: subscribe to comments reloaded -->$html_to_show<!-- END: subscribe to comments reloaded -->";
+	echo "<!-- BEGIN: subscribe to comments reloaded --><p class=\"comment-form-subscriptions\">$html_to_show</p><!-- END: subscribe to comments reloaded -->";
 }
 
 // Show the checkbox - You can manually override this by adding the corresponding function in your template
